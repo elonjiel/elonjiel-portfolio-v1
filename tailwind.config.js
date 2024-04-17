7b3d19b0c23e106df87aba48c8eefce96a9ba6d2
Elon Jiel <elonudemy@gmail.com>
2024-04-17 18:02:44 +0800

    Added Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ export default {
   content: [ "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color": {
+          "50": "#ecf1ff",
+          "100": "#d8e3fe",
+          "200": "#b1c8fd",
+          "300": "#8bacfc",
+          "400": "#6491fb",
+          "500": "#3d75fa",
+          "600": "#315ec8",
+          "700": "#254696",
+          "800": "#182f64",
+          "900": "#0c1732",
+        },
+      },
+      fontFamily: {
+        "fjalla": ["Fjalla One" , "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
